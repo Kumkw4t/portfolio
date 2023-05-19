1b1fd8828cef35b8279317be061023acfc364be4
kumkw4t <36503156+Kumkw4t@users.noreply.github.com>
2023-05-19 14:18:43 +0200

    squelette frise works

--- frontend/src/components/WorkPanel.jsx
+++ frontend/src/components/WorkPanel.jsx
@@ -1,17 +1,23 @@
-import '../styles/WorkPanel.css'
+import '../styles/WorkPanel.css';
+import workList from '../data/works.json';
 
 function WorkPanel ( {workId, isEven}) {
 
+	const work = workList.find( (element) => element.id === workId);
+
 	return(
-		<div className="workpanel-root-container work-container-child">
-		<div className={isEven ? "workpanel-content-container workpanel-content-container-even" : "workpanel-content-container workpanel-content-container-odd"}>
+		<li className={isEven ? "workpanel-root-case workpanel-element even" : "workpanel-root-case workpanel-element odd"}>
+		<div className="workpanel-case-year"><p>{work.year}</p></div>
+		<div className="workpanel-content-perspective">
+		<div className="workpanel-content-case">
 			<div className="workpanel-image">Image</div>
 			<div className="wokpanel-content">
 				<div className="workpanel-title">{workId}</div>
-				<div className="workpanel-text">Texte ultra descriptif</div>
+				<div className="workpanel-text">{work.title}</div>
 			</div>
 		</div>
 		</div>
+		</li>
 	);
 }
 
--- frontend/src/pages/Works.jsx
+++ frontend/src/pages/Works.jsx
@@ -4,40 +4,20 @@ import WorkPanel from '../components/WorkPanel';
 import workList from '../data/works.json'
 
 function Works () {
-
-	const formattedWorkList = [];
-
-	workList.map( (work, index) => {
-		if (index%2 === 1) {
-			formattedWorkList.push({"isWork": false, "isEven": false, "id": null, "year": work.year});
-			formattedWorkList.push({"isWork": true, "isEven": true, "id": work.id, "year": null});
-		} else {
-			formattedWorkList.push({"isWork": true, "isEven": false, "id": work.id, "year": null});
-			formattedWorkList.push({"isWork": false, "isEven": true, "id": null, "year": work.year});
-		}
-	});
-
 	return(
 		<div className="works-global-container">
-			<div className="works-background-container">
 			<div className="svg-circle-top">
 				<svg width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
 					<circle cx="50" cy="50" r="45" fill="none" stroke="black" stroke-width="4"/>
 				</svg>
 			</div>
-			<div className="background-line">
-			</div>
-			</div>
-			<div className="workpanel-global-container">
-				{console.log(formattedWorkList)}
+			<ul className="workpanel-global-container">
 				{
-					formattedWorkList.map( (element) => (
-						element.isWork ? (<WorkPanel workId={element.id} isEven={element.isEven} />)
-						:
-						(<div className={`work-year-container work-container-child work-year-container-${element.isEven ? "even" : "odd"}`}><p>{element.year}</p></div>)
+					workList.map( (work, index) => (
+						<WorkPanel key={work.id} workId={work.id} isEven={index%2 === 0 ? false : true} />
 					))
 				}
-			</div>
+			</ul>
 		</div>
 	);
 }
