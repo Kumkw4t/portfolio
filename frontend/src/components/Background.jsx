149facd843a551696a5543decbf5bd054182b210
kumkw4t <36503156+Kumkw4t@users.noreply.github.com>
2023-07-12 20:20:35 +0200

    start background

--- frontend/src/components/Background.jsx
+++ frontend/src/components/Background.jsx
@@ -1,16 +1,24 @@
 import '../styles/Background.css';
 import {useEffect} from 'react';
 import { PtsCanvas, HandleAnimateFn, HandleStartFn, HandleActionFn } from "react-pts-canvas";
-import {Pt, Create, Util} from "pts";
+import {Pt, Create, Util, Group, Particle, Num} from "pts";
 
 function Background ({children}) {
 
 	let local_pointer = new Pt(0,0);
+	let particles = new Group();
+	const colors = ['#5699e9','#7c85e8','#9478e7','#b268e6','#cf59e6'];
+
+	class Star extends Pt {
+			color = colors[Math.floor(Num.randomRange(0,5))];
+			radius = Num.randomRange(0,1.5);
+			offsetRadius = Num.randomRange(1,5000);
+			offsetSpeed = Num.randomRange(1,5);
+	}
 
 	useEffect(() => {
 	    const getMouseLocation = (e) => {
 	    	local_pointer.to(e.clientX,e.clientY+window.scrollY);
-	    	// console.log(local_pointer);
 	    };
 
 	    document.addEventListener('mousemove',getMouseLocation,true);
@@ -20,8 +28,16 @@ function Background ({children}) {
 	    };
 	});
 
-	const handleAnimate: HandleAnimateFn = (space, form, time) => {
-		form.point(local_pointer, (time%1000)/1000*20, 'circle');
+	const handleStart: HandleStartFn = (_bound, space, form) => {
+		console.log(space.size);
+		console.log(space.width + ' ' + space.height);
+		const points = Create.distributeRandom(space.innerBound,(space.width*space.height*0.00018));
+		particles = Group.fromPtArray(points.map( (pt) => (new Star(pt))));
+	}
+
+	const handleAnimate: HandleAnimateFn = (space, form, time, ftime) => {
+		// form.point(local_pointer, (time%1000)/1000*20, 'circle');
+		particles.map( (particle) => (form.fillOnly(particle.color).point(particle,1+particle.radius*Num.cycle((time+particle.offsetRadius)%2000/2000), 'circle')));
 	}
 
 	return (
@@ -29,6 +45,7 @@ function Background ({children}) {
 		    <PtsCanvas
 		      background="#0e1012"
 		      name="canvas"
+		      onStart={handleStart}
 		      onAnimate={handleAnimate}
 		    />
 		    {children}
